e911816252499d5ba9c403df84cd2be2c56aa850
mr_pugi <mrpugiedp@gmail.com>
2019-11-05 02:02:31 +0900

    (6)(7)終了

--- mysqrt.f90
+++ mysqrt.f90
@@ -1,14 +1,15 @@
 program mysqrt
 
   implicit none
-  integer::n=100,i
-  real::x,ymin,xmin,a,dx
+  integer::n=100,i,fo=10
+  real::x,ymin,xmin,a,dx,dxplt,y
 
   print*,"Let's get min f(x)=x^2-a=0"
   print*,"Please input a"
   read*,a
-  
+  open(fo,file="output.txt")
   dx=a/n
+  dxplt=a/1000
   xmin=0
   ymin=f(0.0,a)**2
   do i=0,n
@@ -16,6 +17,11 @@ program mysqrt
       ymin=f(dx*i,a)**2
       xmin=dx*i
     end if
+  end do
+  do i=0,1000
+    x=dxplt*i
+    y=f(x,a)**2
+    write(fo, '(3e12.4)') x, y
   end do
   print*,"f(",xmin,")=",ymin
 contains
